79426d02e69ccc1e152bff03c4c5e2bf934ad74b
infinith4@gmail.com <infinith4@gmail.com>
2016-10-29 19:04:18 +0900

    add Affine layer

--- layers.py
+++ layers.py
@@ -12,7 +12,23 @@ class Relu:
         dout[self.mask]=0
         dx = dout
         return dx
-        
+class Affine:
+    def __init__(self,W,b):
+        self.W = W
+        self.b = b
+        self.x None
+        self.dw = None
+        self.db = None
+    def forward(self,x):
+        self.x = x
+        out = np.dot(x,self.W) + self.b
+        return out
+    def backward(self,dout):
+        dx = np.dot(dout,self.W.T)
+        self.dw = np.dot(self.x.T,dout)
+        self.db = np.sum(dout,axis=0)
+        return dx
+
 x = np.array([[1.0,-0.5],[-2.0,3.0]])
 print(x)
 
