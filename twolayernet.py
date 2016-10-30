ebbf61c9d539079bc6e0d25e9c76f0a79df11095
infinith4@gmail.com <infinith4@gmail.com>
2016-10-30 17:03:18 +0900

    add gradient check

--- twolayernet.py
+++ twolayernet.py
@@ -7,18 +7,20 @@ from common.gradient import numerical_gradient
 from collections import OrderedDict
 
 class TwoLayerNet:
-    def __init__(self,input_size,hidden_size,output_size,weight_init_std=0.01):
+    def __init__(self, input_size, hidden_size, output_size, weight_init_std = 0.01):
+        # 重みの初期化
         self.params = {}
-        self.params['W1'] = weight_init_std * np.random.randn(input_size,hidden_size)
+        self.params['W1'] = weight_init_std * np.random.randn(input_size, hidden_size)
         self.params['b1'] = np.zeros(hidden_size)
-        self.params['W2'] = weight_init_std * np.random.randn（hidden_size,output_size）
+        self.params['W2'] = weight_init_std * np.random.randn(hidden_size, output_size) 
         self.params['b2'] = np.zeros(output_size)
-        
-        #gengerate layer
+
+        # レイヤの生成
         self.layers = OrderedDict()
-        self.layers['Affine1'] = Affine(self.params['W1'],self.params['b1'])
+        self.layers['Affine1'] = Affine(self.params['W1'], self.params['b1'])
         self.layers['Relu1'] = Relu()
-        self.layers['Affine2'] = Affine(self.params['W2'],self.params['b2'])
+        self.layers['Affine2'] = Affine(self.params['W2'], self.params['b2'])
+
         self.lastLayer = SoftmaxWithLoss()
     def predict(self,x):
         for layer in self.layers.values():
