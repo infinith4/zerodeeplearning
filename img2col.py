71e26b24c6fdcb23450c3998b753b40f20b733ff
infinith4@gmail.com <infinith4@gmail.com>
2016-11-12 15:43:47 +0900

    add img2col

---
+++ img2col.py
@@ -0,0 +1,7 @@
+import os, sys
+sys.path.append(os.curdir)
+from common.util import im2col
+import numpy as np
+x1 = np.random.rand(1,3,7,7)
+col1 = im2col(x1,5,5,stride=1,pad=0)
+print(col1.shape) #(9, 75)
