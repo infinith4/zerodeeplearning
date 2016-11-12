4b9d592f464da511b772d8231aec61535b41c5e6
infinith4@gmail.com <infinith4@gmail.com>
2016-11-13 00:42:26 +0900

    CNN training result

--- train_convnet.py
+++ train_convnet.py
@@ -13,13 +13,13 @@ from common.trainer import Trainer
 # 処理に時間のかかる場合はデータを削減 
 #x_train, t_train = x_train[:5000], t_train[:5000]
 #x_test, t_test = x_test[:1000], t_test[:1000]
-
+max_epochs = 20
 network = SimpleConvNet(input_dim=(1,28,28), 
                         conv_param = {'filter_num': 30, 'filter_size': 5, 'pad': 0, 'stride': 1},
                         hidden_size=100, output_size=10, weight_init_std=0.01)
                         
 trainer = Trainer(network, x_train, t_train, x_test, t_test,
-                  epochs=20, mini_batch_size=100,
+                  epochs=max_epochs, mini_batch_size=100,
                   optimizer='Adam', optimizer_param={'lr': 0.001},
                   evaluate_sample_num_per_epoch=1000)
 trainer.train()
@@ -30,9 +30,10 @@ print("Saved Network Parameters!")
 
 # グラフの描画
 markers = {'train': 'o', 'test': 's'}
+
 x = np.arange(max_epochs)
-plt.plot(x, train_acc_list, marker='o', label='train', markevery=2)
-plt.plot(x, test_acc_list, marker='s', label='test', markevery=2)
+plt.plot(x, trainer.train_acc_list, marker='o', label='train', markevery=2)
+plt.plot(x, trainer.test_acc_list, marker='s', label='test', markevery=2)
 plt.xlabel("epochs")
 plt.ylabel("accuracy")
 plt.ylim(0, 1.0)
